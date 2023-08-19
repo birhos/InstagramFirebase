dd06ec1fdba4774c76c48c3cfccd0428433ae28f
birhos <haydarrdemir@hotmail.comhaydarrdemir@hotmail.comhaydarrdemir@hotmail.com>
2023-08-19 07:57:39 +0300

    First Commit

---
+++ InstagramFirebase/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramFirebase
+//
+//  Created by Haydar Demir on 19.08.2023.
+//
+
+import UIKit
+
+final class MainTabController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .yellow
+    }
+}
---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by Haydar Demir on 19.08.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+}
