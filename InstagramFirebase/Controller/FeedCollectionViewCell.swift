fa332f920f46ae43e7d9535d8a188fce9e28cc3f
birhos <haydarrdemir@hotmail.comhaydarrdemir@hotmail.comhaydarrdemir@hotmail.com>
2023-08-20 10:02:54 +0300

    Finishing Up Feed Cell UI

--- InstagramFirebase/Controller/FeedCollectionViewCell.swift
+++ InstagramFirebase/Controller/FeedCollectionViewCell.swift
@@ -29,6 +29,63 @@ class FeedCollectionViewCell: UICollectionViewCell {
         button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
         return button
     }()
+    
+    private lazy var postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        imageView.image = UIImage(named: "venom-7")
+        return imageView
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "like_unselected"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var commentButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "comment"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var shareButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "send2"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var likesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1 like"
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        return label
+    }()
+    
+    private lazy var captionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Some text caption for now..."
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    private lazy var postTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2 days ago"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        return stackView
+    }()
 
     // MARK: Lifecycle
     
@@ -39,19 +96,47 @@ class FeedCollectionViewCell: UICollectionViewCell {
         
         addSubview(profileImageView)
         addSubview(usernameButton)
+        addSubview(postImageView)
         
         profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
         profileImageView.setDimensions(height: 40, width: 40)
         profileImageView.layer.cornerRadius = 40 / 2
         
         usernameButton.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+        
+        postImageView.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 8)
+        postImageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
+        
+        configureActionButtons()
+        
+        addSubview(likesLabel)
+        likesLabel.anchor(top: likeButton.bottomAnchor, left: leftAnchor, paddingTop: -4, paddingLeft: 8)
+        
+        addSubview(captionLabel)
+        captionLabel.anchor(top: likesLabel.bottomAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 8)
+        
+        addSubview(postTimeLabel)
+        postTimeLabel.anchor(top: captionLabel.bottomAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 8)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: Actions
+    
     @objc func didTapUsername() {
         print("DEBUG: did tap username")
+    }
+    
+    // MARK: Helpers
+    
+    private func configureActionButtons() {
+        stackView = UIStackView(arrangedSubviews: [likeButton, commentButton, shareButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        
+        addSubview(stackView)
+        stackView.anchor(top: postImageView.bottomAnchor, width: 120, height: 50)
     }
 }
--- InstagramFirebase/Controller/FeedController.swift
+++ InstagramFirebase/Controller/FeedController.swift
@@ -46,7 +46,7 @@ extension FeedController {
 extension FeedController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width: CGFloat = view.frame.width
-        let height: CGFloat = 200
+        let height: CGFloat = width + 8 + 40 + 8 + 50 + 60
         return CGSize(width: width, height: height)
     }
 }
